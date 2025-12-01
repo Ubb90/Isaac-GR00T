c4f2486d196c003fccddbdd6477570d9beb69ca1
Ubb90 <florent.audonnet@glasgow.ac.uk>
2025-12-01 17:49:43 +0000

    Add short and medium horizon data configuration classes for So100Track

--- gr00t/experiment/data_config.py
+++ gr00t/experiment/data_config.py
@@ -317,6 +317,10 @@ class So100TrackDataConfig(BaseDataConfig):
 
 class So100TrackLongHorizonDataConfig(So100TrackDataConfig):
     action_indices = list(range(150))
+class So100TrackShortHorizonDataConfig(So100TrackDataConfig):
+    action_indices = list(range(8))
+class So100TrackMediumHorizonDataConfig(So100TrackDataConfig):
+    action_indices = list(range(32))
 
 
 ###########################################################################################
@@ -842,6 +846,8 @@ DATA_CONFIG_MAP = {
     "so100": So100DataConfig(),
     "so100_track": So100TrackDataConfig(),
     "so100_track_long": So100TrackLongHorizonDataConfig(),
+    "so100_track_short": So100TrackShortHorizonDataConfig(),
+    "so100_track_medium": So100TrackMediumHorizonDataConfig(),
     "so100_dualcam": So100DualCamDataConfig(),
     "unitree_g1": UnitreeG1DataConfig(),
     "unitree_g1_full_body": UnitreeG1FullBodyDataConfig(),
